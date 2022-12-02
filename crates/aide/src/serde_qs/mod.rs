75d8abc619884aef6ca75274c6c0e8fa28a9f506
Dennis Kievits <denkievits@gmail.com>
2022-12-02 18:33:04 +0100

    feat(serde_qs): doc add JsonSchema derive

--- crates/aide/src/serde_qs/mod.rs
+++ crates/aide/src/serde_qs/mod.rs
@@ -2,11 +2,12 @@
 //!
 //! Enabling this feature allow you to use `QsQuery` as an input for your handlers to be used within Aide's routing functions.
 //! ```no_run
+//! use schemars::JsonSchema;
 //! use serde::Deserialize;
 //! use serde_qs::axum::QsQuery;
 //! use aide::axum::IntoApiResponse;
 //!
-//! #[derive(Deserialize)]
+//! #[derive(Deserialize, JsonSchema)]
 //! struct MyQuery {
 //!     sequence: Vec<i32>,
 //! }
